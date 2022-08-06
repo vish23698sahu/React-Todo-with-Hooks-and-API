798c44441f7674ebcf8eba47de05de67f88d5c48
Vishakha Sahu <vishakhasahu023@gmail.com>
2022-08-06 19:20:22 +0530

    version - 2 clean up for delete

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,14 @@
 import { Fragment } from "react";
 
 const TodoList = (props) => {
+    //Below code is not correct. Hence commented
+    // const onDeleteHandler = () => {
+    //     console.log('Delete clicked');
+    //     fetch('https://todo-react-portfolio-default-rtdb.firebaseio.com/TodoLost.json/0', {
+    //         method: 'DELETE'
+    //     });
+    // }
+
     return (
         <Fragment>
             <ul>
